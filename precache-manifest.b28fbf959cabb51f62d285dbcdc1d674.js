ca7ddee02c11b7dc3891e7ce6e81df8eb7e0cee6
Justin_Jung <jungdw0624@gmail.com>
2019-05-30 13:00:52 +0900

    Updates

---
+++ precache-manifest.b28fbf959cabb51f62d285dbcdc1d674.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3a22b5ed5cc4cac0e4bf73a90a0b109",
+    "url": "/bible_commentary/index.html"
+  },
+  {
+    "revision": "83e11c28eb41218b20f4",
+    "url": "/bible_commentary/static/css/main.6e5edbf2.chunk.css"
+  },
+  {
+    "revision": "31d68d79c0937db6dad4",
+    "url": "/bible_commentary/static/js/2.fb03e36a.chunk.js"
+  },
+  {
+    "revision": "83e11c28eb41218b20f4",
+    "url": "/bible_commentary/static/js/main.26048a2e.chunk.js"
+  },
+  {
+    "revision": "b051040a3a9725922930",
+    "url": "/bible_commentary/static/js/runtime~main.95e5ced6.js"
+  }
+]);
