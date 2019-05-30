c09b94e71a5a653598e42cc3d84f3b1d8175e5c1
Justin_Jung <jungdw0624@gmail.com>
2019-05-30 11:03:59 +0900

    back to the beginning

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
-import PDFViewer from './pdfviewer';
-import PDFJSBackend from './backend/pdfjs';
-import WebviewerBackend from './backend/webviewer';
 
 class App extends Component{
 
-  constructor(){
-    super();
-    this.myViewer = React.createRef();
-  }
 
-  render(){
-    return (
-      <div className="App">
-        <PDFViewer backend={WebviewerBackend} 
-          src='/CSS3.pdf' />
-      </div>
-    )
-  }
+  
 }
 
 export default App;
